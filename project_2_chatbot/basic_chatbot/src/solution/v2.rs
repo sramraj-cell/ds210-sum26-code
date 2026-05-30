70512f906c82e7665a7644118202303456c33950
Shiva Ramraj <sramraj@bu.edu>
2026-05-30 17:52:26 -0400

    Session stored inside the struct so it does not get lost when called again, there is memory.

--- project_2_chatbot/basic_chatbot/src/solution/v2.rs
+++ project_2_chatbot/basic_chatbot/src/solution/v2.rs
@@ -2,6 +2,7 @@ use kalosm::language::*;
 
 #[allow(dead_code)]
 pub struct ChatbotV2 {
+    chat_session: Chat<Llama>,
     // What should you store inside your Chatbot type?
     // The model? The chat_session?
 }
@@ -10,6 +11,9 @@ impl ChatbotV2 {
     #[allow(dead_code)]
     pub fn new(model: Llama) -> ChatbotV2 {
         return ChatbotV2 {
+            chat_session: model
+              .chat()
+              .with_system_prompt("The assistant will act like a drunk comedian"),
             // Whatever you decide to store in the struct
             // you need to make sure you pass here!
         };
@@ -17,7 +21,10 @@ impl ChatbotV2 {
 
     #[allow(dead_code)]
     pub async fn chat_with_user(&mut self, message: String) -> String {
+        match self.chat_session.add_message(message).await {
+            Ok(response) => response,
+            Err(_) => String::from("Error generating response"),
+        }
         // Add your code for chatting with the agent while keeping conversation history here.
-        return String::from("Hello, I am not a bot (yet)!");
     }
 }
