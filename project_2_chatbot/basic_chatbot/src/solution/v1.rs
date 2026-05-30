9f5feabd0d48a288eeab05f2d5f431fb3d8764e2
Shiva Ramraj <sramraj@bu.edu>
2026-05-30 17:42:33 -0400

    Fully working responses, changed system to sound like a drunk comedian

--- project_2_chatbot/basic_chatbot/src/solution/v1.rs
+++ project_2_chatbot/basic_chatbot/src/solution/v1.rs
@@ -13,15 +13,18 @@ impl ChatbotV1 {
 
     #[allow(dead_code)]
     pub async fn chat_with_user(&mut self, message: String) -> String {
-        let mut chat_session: Chat<Llama> = self.model
-            .chat()
-            .with_system_prompt("The assistant will act like a pirate");
+    let mut chat_session: Chat<Llama> = self.model
+        .chat()
+        .with_system_prompt("The assistant will act like a drunk comedian");
+
+    match chat_session.add_message(message).await {
+        Ok(response) => response,
+        Err(_) => String::from("Error generating response"),
+    }
+}
 
         // You need to add your code here
         // You must find a way to add the given message to the chat_session!
         // consider https://docs.rs/kalosm/0.4.0/kalosm/language/struct.Chat.html#method.add_message
         // Hint: make sure you transform/extract the response message as a **String**.
-
-        return String::from("Hello, I am not a bot (yet)!");
     }
-}
