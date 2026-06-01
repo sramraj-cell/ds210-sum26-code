85f42516e5050a10c75796ab7d0de96a3e33012a
Shiva Ramraj <sramraj@bu.edu>
2026-05-31 21:27:44 -0400

    fixed issues with (with_sesion)

--- project_2_chatbot/cache_chatbot/src/solution/v5.rs
+++ project_2_chatbot/cache_chatbot/src/solution/v5.rs
@@ -18,26 +18,55 @@ impl ChatbotV5 {
     }
 
     pub async fn chat_with_user(&mut self, username: String, message: String) -> String {
-        let filename = &format!("{}.txt", username);
+        let filename = format!("{}.txt", username);
         let cached_chat = self.cache.get_mut(&username);
 
         match cached_chat {
             None => {
                 println!("chat_with_user: {username} is not in the cache!");
                 // The cache does not have the chat. What should you do?
-                return String::from("Hello, I am not a bot (yet)!");
+
+                let mut chat_session = self.model.chat()
+                    .with_system_prompt("The assistant will act like a pirate");
+
+                if let Some(session) = file_library::load_chat_session_from_file(&filename) {
+                    chat_session = fixed_load_session(
+                        self.model.chat().with_system_prompt("The assistant will act like a pirate"),
+                        session
+                    );
+                }
+
+                let response = match chat_session.add_message(message).await {
+                    Ok(r) => r,
+                    Err(_) => String::from("Error generating response"),
+                };
+
+                if let Ok(session) = chat_session.session() {
+                    file_library::save_chat_session_to_file(&filename, &session);
+                }
+                self.cache.put(username, chat_session);
+
+                return response;
             }
             Some(chat_session) => {
                 println!("chat_with_user: {username} is in the cache! Nice!");
                 // The cache has this chat. What should you do?
-                return String::from("Hello, I am not a bot (yet)!");
 
+                let response = match chat_session.add_message(message).await {
+                    Ok(r) => r,
+                    Err(_) => String::from("Error generating response"),
+                };
+
+                if let Ok(session) = chat_session.session() {
+                    file_library::save_chat_session_to_file(&filename, &session);
+                }
+                return response;
             }
         }
     }
 
     pub fn get_history(&mut self, username: String) -> Vec<String> {
-        let filename = &format!("{}.txt", username);
+        let filename = format!("{}.txt", username);
         let cached_chat = self.cache.get_mut(&username);
 
         match cached_chat {
@@ -45,14 +74,39 @@ impl ChatbotV5 {
                 println!("get_history: {username} is not in the cache!");
                 // TODO: The cache does not have the chat. What should you do?
                 // Your code goes here.
-                return Vec::new();
+
+                let session = file_library::load_chat_session_from_file(&filename);
+                if session.is_none() {
+                    return Vec::new();
+                }
+                let session = session.unwrap();
+
+                let history = session.history().iter()
+                    .filter(|m| m.role() != MessageType::SystemPrompt)
+                    .map(|m| m.content().to_string())
+                    .collect();
+
+                let chat_session = fixed_load_session(
+                    self.model.chat().with_system_prompt("The assistant will act like a pirate"),
+                    session
+                );
+                self.cache.put(username, chat_session);
+
+                return history;
             }
             Some(chat_session) => {
                 println!("get_history: {username} is in the cache! Nice!");
                 // TODO: The cache has this chat. What should you do?
                 // Your code goes here.
-                return Vec::new();
 
+                let session = chat_session.session();
+                if session.is_err() {
+                    return Vec::new();
+                }
+                return session.unwrap().history().iter()
+                    .filter(|m| m.role() != MessageType::SystemPrompt)
+                    .map(|m| m.content().to_string())
+                    .collect();
             }
         }
     }
--- project_2_chatbot/file_chatbot/src/solution/v4.rs
+++ project_2_chatbot/file_chatbot/src/solution/v4.rs
@@ -14,14 +14,17 @@ impl ChatbotV4 {
     }
 
     pub async fn chat_with_user(&mut self, username: String, message: String) -> String {
-        let filename = &format!("{}.bin", username);
+        let filename = &format!("{}.txt", username);
 
         let mut chat_session: Chat<Llama> = self.model
             .chat()
             .with_system_prompt("The assistant will act like a pirate");
 
         if let Some(session) = file_library::load_chat_session_from_file(filename) {
-            chat_session = fixed_load_session(chat_session, session);
+            chat_session = fixed_load_session(
+                self.model.chat().with_system_prompt("The assistant will act like a pirate"),
+                session
+            );
         }
 
         let response = match chat_session.add_message(message).await {
@@ -42,7 +45,7 @@ impl ChatbotV4 {
     }
 
     pub fn get_history(&self, username: String) -> Vec<String> {
-        let filename = &format!("{}.bin", username);
+        let filename = &format!("{}.txt", username);
 
         match file_library::load_chat_session_from_file(&filename) {
             None => {
@@ -52,7 +55,7 @@ impl ChatbotV4 {
                 session
                     .history()
                     .iter()
-                    .filter(|message| message.role() != MessageType::SystemPrompt)
+                    .filter(|m| m.role() != MessageType::SystemPrompt)
                     .map(|message| message.content().to_string())
                     .collect()
                 // TODO: what should happen here?
